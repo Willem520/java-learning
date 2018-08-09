23c0be9aae41b71d105144d0ad9525021cdbe795
weiyu <weiyu@gomefinance.com.cn>
2018-08-09 14:11:11 +0800

    添加钩子方法示例代码

---
+++ src/main/java/willem/weiyu/hook/HookDemo.java
@@ -0,0 +1,17 @@
+package willem.weiyu.hook;
+
+/**
+ * @author weiyu
+ * @description
+ * @create 2018/8/9 14:06
+ * @since 1.0.0
+ */
+public class HookDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Runtime.getRuntime().addShutdownHook(new Thread(()-> System.out.println("this is a shutdown hook")));
+        System.out.println("======模拟程序执行");
+        Thread.sleep(5000);
+        System.out.println("hello hook");
+    }
+}
