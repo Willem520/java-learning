3bb3aa4c237e936c17e996991e78b326684274fb
weiyu <weiyu005@ke.com>
2019-04-30 12:13:32 +0800

    添加CompletableFuture测试示例

---
+++ src/main/java/willem/weiyu/concurrent/CompletableFutureDemo.java
@@ -0,0 +1,12 @@
+package willem.weiyu.concurrent;
+
+/**
+ * @Author weiyu
+ * @Description
+ * @Date 2019/4/29 17:16
+ * @since jdk1.8
+ * @see CompletableFutureTest
+ */
+public class CompletableFutureDemo {
+
+}
